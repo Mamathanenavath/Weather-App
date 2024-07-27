46fe813aa0e5ea1d364cd5c2f6fc3d87f2d63315
MamathaMunni <mamathanenavath137@gmail.com>
2024-07-27 17:09:06 +0530

    script.js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// const inputBox = document.querySelector('.input-box');
-// const searchBtn = document.querySelector('searchBtn');
-// const weather_img = document.querySelector('.weather-img');
-// const temperature = document.querySelector('.temperature');
-// const description = document.querySelector('.description');
-// const humidity = document.querySelector('humidity');
-// const wind_speed = document.querySelector('wind-speed');
-
-
-// async function checkWeather(city){
-//     const api_key = "54b1cf45d9ad4f088ecbc03018e33b1b";
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-
-//     const weather_data = await fetch(`${url}`).then(response => response.json());
-
-//     console.log(weather_data);
-// }
-
-
-// searchBtn.addEventListener('click', ()=>{
-//     checkWeather(inputBox.value);
-// });
-
-
-
 const inputBox = document.querySelector('.input-box');
 const searchBtn = document.getElementById('searchBtn');
 const weather_img = document.querySelector('.weather-img');
